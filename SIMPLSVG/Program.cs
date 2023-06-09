b526ba0b7daded5dc740865b5ab105b19ff7da4f
Aviv Cohn <acohn@dgitech.com>
2023-06-09 10:06:25 -0400

    Updating gitignore

--- SIMPLSVG/Program.cs
+++ SIMPLSVG/Program.cs
@@ -2,16 +2,46 @@
 
 using System.Drawing;
 using Svg;
-Console.WriteLine("Drag the folder containing the SVG file here, then press enter");
-var path = Console.ReadLine();
 
-var svgDocument = SvgDocument.Open(path);
-var svgPath = svgDocument.Children[0] as SvgPath;
-//Change color of an SVG
-svgPath.Fill = new SvgColourServer(Color.Blue);
+Color _color;
 
-//save the svg to a new file
-svgDocument.Write("svgtest.svg");
-Console.WriteLine("success?");
-Console.ReadKey();
+string promptMessage(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
 
+
+var path = promptMessage("Drag the folder containing the SVG file here, then press enter").Replace("\"", "");
+
+var response = promptMessage("What color would you like to use?");
+
+if (response.Contains('#'))
+{
+    _color = ColorTranslator.FromHtml(response);
+}
+else
+{
+    _color = Color.FromName(response);
+}
+
+string[] svgFiles = Directory.GetFiles(path, "*.svg");
+
+
+foreach (string file in svgFiles)
+{
+    ConvertColor(file, _color);
+}
+
+
+static void ConvertColor(string path, Color color)
+{
+    var svgDocument = SvgDocument.Open(Path.GetFullPath(path));
+
+    var svgPath = svgDocument.Children[0] as SvgPath;
+    //Change color of an SVG
+    svgPath.Fill = new SvgColourServer(color);
+
+    //save the svg to a new file
+    svgDocument.Write($"{Path.GetDirectoryName(path)}/temp_new.svg");
+}
