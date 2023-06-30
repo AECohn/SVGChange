5bb051bb654f9a581bd2ac8c5a59213e9ede3405
Aviv Cohn <acohn@dgitech.com>
2023-06-30 15:29:57 -0400

    More cleanup

---
+++ SVGChange/InternalMethods.cs
@@ -0,0 +1,64 @@
+﻿using Svg;
+
+namespace SIMPLSVG;
+
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class InternalMethods
+{
+    static public string Prompt(string message)
+    {
+        Console.WriteLine(message);
+        return Console.ReadLine() ?? string.Empty;
+    }
+
+    static public int GetDimension(string message)
+    {
+        int result;
+        while (true)
+        {
+            if (int.TryParse(Prompt(message), out result))
+            {
+                return result;
+            }
+
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+
+    static public Color ParseColor(string input)
+    {
+        if (input.Contains('#'))
+        {
+            return ColorTranslator.FromHtml(input);
+        }
+        else
+        {
+            Color color = Color.FromName(input);
+            if (color.IsKnownColor)
+            {
+                return color;
+            }
+            else
+            {
+                Console.WriteLine("Unknown color, setting to black.");
+                return Color.Black;
+            }
+        }
+    }
+
+    static public SvgDocument ConvertColor(string svgFile, Color color)
+    {
+        SvgDocument svgDocument = SvgDocument.Open(svgFile);
+        svgDocument.Fill = new SvgColourServer(color);
+        return svgDocument;
+    }
+
+    static public void ConvertSVG(SvgDocument SVGDoc, string outputPath, Bitmap bitmap, ImageFormat imageFormat)
+    {
+        SVGDoc.Draw(bitmap);
+        bitmap.Save(outputPath, imageFormat);
+    }
+}
+    
--- SVGChange/SVGChange.cs
+++ SVGChange/SVGChange.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
+using SIMPLSVG;
 using Svg;
 
 class SVGChange
@@ -10,13 +9,20 @@ class SVGChange
     {
         try
         {
-            string path = Prompt("Drag the folder containing the SVG file(s) here, then press enter").Replace("\"", "");
+            string path = InternalMethods.Prompt("Drag the folder containing the SVG file(s) here, then press enter")
+                .Replace("\"", "");
             ;
-            string colorInput = Prompt("What color would you like to use?");
-            Color color = ParseColor(colorInput);
-            bool useCustomDimensions = Prompt("Would you like to use custom dimensions? (y/n)").ToLower() == "y"? true: false;
-            int width = useCustomDimensions ? GetDimension("What width in pixels would you like to use?") : 0;
-            int height = useCustomDimensions ? GetDimension("What height in pixels would you like to use?") : 0;
+            string colorInput = InternalMethods.Prompt("What color would you like to use?");
+            Color color = InternalMethods.ParseColor(colorInput);
+            bool useCustomDimensions =
+                InternalMethods.Prompt("Would you like to use custom dimensions? (y/n)").ToLower() == "y";
+            int width = useCustomDimensions
+                ? InternalMethods.GetDimension("What width in pixels would you like to use?")
+                : 0;
+            int height = useCustomDimensions
+                ? InternalMethods.GetDimension("What height in pixels would you like to use?")
+                : 0;
+            Bitmap bitmap;
 
             Console.WriteLine("Converting...");
 
@@ -27,18 +33,12 @@ class SVGChange
 
             for (int i = 0; i < svgFiles.Length; i++)
             {
-                SvgDocument svgDocument = ConvertColor(svgFiles[i], color);
-                SvgPath svgPath = (SvgPath)svgDocument.Children[0];
-
-                Bitmap bitmap = useCustomDimensions
-                    ? new Bitmap(width, height)
-                    : new Bitmap((int)svgPath.Bounds.Width, (int)svgPath.Bounds.Height);
-
-                svgDocument.Draw(bitmap);
-
-                string outputAddress =
-                    $"{Path.GetDirectoryName(svgFiles[i])}/{subfolderName}/{Path.GetFileNameWithoutExtension(svgFiles[i])}_{color.Name}.png";
-                bitmap.Save(outputAddress, ImageFormat.Png);
+                SvgDocument svgDocument = InternalMethods.ConvertColor(svgFiles[i], color);
+                bitmap = useCustomDimensions? new Bitmap(width, height) : new Bitmap((int)svgDocument.Width, (int)svgDocument.Height);
+               
+                InternalMethods.ConvertSVG(svgDocument,
+                    $"{Path.GetDirectoryName(svgFiles[i])}/{subfolderName}/{Path.GetFileNameWithoutExtension(svgFiles[i])}_{color.Name}.png",
+                    bitmap, ImageFormat.Png);
             }
 
             Console.WriteLine("Done.");
@@ -47,53 +47,5 @@ class SVGChange
         {
             Console.WriteLine($"Error: {ex.Message}");
         }
-    }
-
-    static string Prompt(string message)
-    {
-        Console.WriteLine(message);
-        return Console.ReadLine() ?? string.Empty;
-    }
-
-    static int GetDimension(string message)
-    {
-        int result;
-        while (true)
-        {
-            if (int.TryParse(Prompt(message), out result))
-            {
-                return result;
-            }
-
-            Console.WriteLine("Invalid input. Please enter a valid integer.");
-        }
-    }
-
-    static Color ParseColor(string input)
-    {
-        if (input.Contains('#'))
-        {
-            return ColorTranslator.FromHtml(input);
-        }
-        else
-        {
-            Color color = Color.FromName(input);
-            if (color.IsKnownColor)
-            {
-                return color;
-            }
-            else
-            {
-                Console.WriteLine("Unknown color, setting to black.");
-                return Color.Black;
-            }
-        }
-    }
-
-    static SvgDocument ConvertColor(string svgFile, Color color)
-    {
-        SvgDocument svgDocument = SvgDocument.Open(svgFile);
-        svgDocument.Fill = new SvgColourServer(color);
-        return svgDocument;
     }
 }
